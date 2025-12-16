63337d313d7a429b180b969d4c4e86d9c9c88d6d
sunak <sunakshigawas335@gmail.com>
2025-12-16 18:05:37 +0530

    crete multiple page

--- ecommerce_project/src/App.jsx
+++ ecommerce_project/src/App.jsx
@@ -1,10 +1,16 @@
+import { Routes, Route} from 'react-router'
 import { HomePage } from './pages/HomePage'
 import './App.css'
 
 function App() {
 
   return (
-    <HomePage />
+    <Routes>
+      <Route path="/" element={<HomePage />}/> 
+      <Route path="checkout" element={<div>checkout test page</div>}/> 
+    
+    </Routes>
+   
   )
 }
 
