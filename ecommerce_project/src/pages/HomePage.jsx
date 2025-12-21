fb41ad76d8fdc0718466d7909f88615119d0522a
sunak <sunakshigawas335@gmail.com>
2025-12-21 11:53:48 +0530

    update the code

--- ecommerce_project/src/pages/HomePage.jsx
+++ ecommerce_project/src/pages/HomePage.jsx
@@ -3,10 +3,11 @@ import './HomePage.css';
 
 export function HomePage() {
     return (
-    <> 
-         <div className="header"> 
+        <> 
+     <title>Ecommerce Project </title>
+    <div className="header"> 
         <div className="left-section">
-            <a href="index.html" className="header-link">
+            <a href="/" className="header-link">
             <img className="logo"
                 src="images/logo-white.png"/>
             <img className="mobile-logo"
@@ -23,12 +24,12 @@ export function HomePage() {
         </div>
 
         <div className="right-section">
-            <a className="orders-link header-link" href="orders.html">
+            <a className="orders-link header-link" href="/orders">
 
             <span className="orders-text">Orders</span>
             </a>
 
-            <a className="cart-link header-link" href="checkout.html">
+            <a className="cart-link header-link" href="/checkout">
             <img className="cart-icon" src="images/icons/cart-icon.png" />
             <div className="cart-quantity">3</div>
             <div className="cart-text">Cart</div>
@@ -155,7 +156,7 @@ export function HomePage() {
             </div>
 
             <div className="product-price">
-                $7.99
+                100
             </div>
 
             <div className="product-quantity-container">
